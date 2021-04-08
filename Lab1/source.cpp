22b3e224c367064673c747d8a97a986f8859a29d
Kirill2002 <kiril20t@gmail.com>
2021-04-09 01:48:57 +0300

    Added a Node struct definition

--- Lab1/source.cpp
+++ Lab1/source.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
+struct Node
+{
+	T m_data;
+	Node<T>* m_next;
+
+	Node();
+	Node(T data, Node<T> next = nullptr);
+}
+
 int main()
 {
 	
