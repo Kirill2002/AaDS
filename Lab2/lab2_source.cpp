cd8a533265b8014dd49909470ed2f2d083d92412
Kirill2002 <kiril20t@gmail.com>
2021-04-23 04:29:26 +0300

    Added BBST definition

--- Lab2/lab2_source.cpp
+++ Lab2/lab2_source.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
+class BBST
+{
+private:
+	T key;
+	int height;
+	BBST* left;
+	BBST* right;
+public:
+	BBST();
+	~BBST();
+	
+	void PrintSorted();
+	int CountNode();
+	int SumKeys();
+	void DeleteEven();
+	BBST* FindMiddle();
+	T FindSecondLargest();
+	BBST* CopyBBST();
+	BBST* InsertBBST(const BBST& a);
+	bool ContainsBBST(const BBST& a);
+	bool IsBalanced();
+	bool EqualsBBST(const BBST& a);
+	BBST* SymetricalBBST();
+	T FatherNode(T k);
+	int CommonAncestor(T a, T b);
+};
+
 int main()
 {
 	
