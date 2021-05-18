1206958d379eb06d20e5415aaf8b1889bac8c875
Kirill2002 <kiril20t@gmail.com>
2021-05-18 21:16:29 +0300

    Added settleRoot private method to ArrayList class

--- Lab3/lab_source.cpp
+++ Lab3/lab_source.cpp
@@ -8,6 +8,25 @@ private:
 	T* array;
 	int last;
 	int max_length;
+
+	void settleRoot(int root, int last)
+	{
+		int child, unsettled = root;
+		while(2 * unsettled + 1 <= last)
+		{
+			if(2 * unsettled + 1 < last &&
+			 array[2 * unsettled + 2] > array[2 * unsettled + 1])
+				child = 2 * unsettled + 2;
+			else 
+				child = 2 * unsettled + 1;
+
+			if(array[unsettled] < array[child])
+			{
+				swap(array[unsettled], array[child]);
+				unsettled = child;
+			}else break;
+		}
+	}
 public:
 	ArrayList(int max = 100)
 	{
@@ -58,5 +77,15 @@ public:
 
 int main()
 {
-
+	ArrayList<int> a(200);
+	a.addItem(10);
+	a.addItem(4);
+	a.addItem(8);
+	a.addItem(9);
+	a.addItem(15);
+	a.addItem(35);
+	a.addItem(5);
+	a.addItem(7);
+	a.printList();
+	cout << '\n';
 }
