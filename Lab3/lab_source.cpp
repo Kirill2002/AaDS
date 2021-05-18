ccb44b38888807e35c2203217fd1bed0be967e27
Kirill2002 <kiril20t@gmail.com>
2021-05-18 20:15:36 +0300

    Created ArrayList definition

---
+++ Lab3/lab_source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class ArrayList
+{
+private:
+	T* array;
+	int last;
+public:
+	ArrayList();
+	~ArrayList();
+	
+};
+
+int main()
+{
+
+}
