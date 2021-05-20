a8f484282faed7c3a3997fdc0e9e6d8471518a24
Kirill2002 <kiril20t@gmail.com>
2021-05-21 01:23:18 +0300

    Added constructor

--- Lab4/lab_source.cpp
+++ Lab4/lab_source.cpp
@@ -5,8 +5,21 @@ class GraphWeighted
 {
 private:
 	pair<int, pair<int, int>>* graph;
+	int v, m;
 public:
-	GraphWeighted();
+	GraphWeighted()
+	{
+		cout << "Input number of vertices and number of edges:\n";
+		cin >> v >> m;
+		graph = new pair<int, pair<int, int>>[m];
+		cout << "Now input all " << m << "edges in format: weight vertice1 vertice2,\n";
+		for(int i = 0; i < m; ++i)
+		{
+			int w, u, v;
+			cin >> w >> u >> v;
+			graph[i] = {w, {u, v}};
+		}
+	}
 	~GraphWeighted();
 	void printGraph();
 	GraphWeighted Kruskal(); 
